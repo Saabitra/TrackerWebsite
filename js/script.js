4ffc5d3b4c72e1cf8dc0bfe9176e33a409e8239d
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2022-09-11 20:11:49 +0530

    update

--- js/script.js
+++ js/script.js
@@ -60,3 +60,20 @@ function reset() {
   counters.forEach((counter) => (counter.innerHTML = '0'));
 }
 
+const parallaxEls = document.querySelectorAll("[data-speed]");
+
+window.addEventListener("scroll", scrollHandler);
+
+function scrollHandler() {
+  for (const parallaxEl of parallaxEls) {
+    const direction = parallaxEl.dataset.direction == "up" ? "-" : "";
+    const transformY = this.pageYOffset * parallaxEl.dataset.speed;
+    if (parallaxEl.classList.contains("banner-title")) {
+      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-6deg)`;
+    } else if (parallaxEl.classList.contains("banner-subtitle")) {
+      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-3deg)`;
+    } else {
+      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
+    }
+  }
+}
