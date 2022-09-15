de21a81f989bb87bcf7d25ca8ec116233e7c9694
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2022-09-15 11:10:28 +0530

    html update

--- js/script.js
+++ js/script.js
@@ -7,8 +7,6 @@ let scrollStarted = false;
 btn.addEventListener('click', navToggle);
 document.addEventListener('scroll', scrollPage);
 
-var video = document.getElementById('myVideo');
-
 function navToggle() {
   btn.classList.toggle('open');
   overlay.classList.toggle('overlay-show');
@@ -58,22 +56,4 @@ function countUp() {
 
 function reset() {
   counters.forEach((counter) => (counter.innerHTML = '0'));
-}
-
-const parallaxEls = document.querySelectorAll("[data-speed]");
-
-window.addEventListener("scroll", scrollHandler);
-
-function scrollHandler() {
-  for (const parallaxEl of parallaxEls) {
-    const direction = parallaxEl.dataset.direction == "up" ? "-" : "";
-    const transformY = this.pageYOffset * parallaxEl.dataset.speed;
-    if (parallaxEl.classList.contains("banner-title")) {
-      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-6deg)`;
-    } else if (parallaxEl.classList.contains("banner-subtitle")) {
-      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0) rotate(-3deg)`;
-    } else {
-      parallaxEl.style.transform = `translate3d(0,${direction}${transformY}px,0)`;
-    }
-  }
 }
