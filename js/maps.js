3f9d8eb094b51298501b005c06131b437955cc46
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2022-10-23 11:27:55 +0530

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -1,4 +1,8 @@
-
+/**
+ * Moves the map to display over Berlin
+ *
+ * @param  {H.Map} map      A HERE Map instance within the application
+ */
  function moveMapToBerlin(map){
     map.setCenter({lat:52.5159, lng:13.3777});
     map.setZoom(14);
